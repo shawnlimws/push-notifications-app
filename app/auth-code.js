83da78092177e6ef485703238ee05c8a1638c3ab
Shawn Lim <shawnlim.ws@gmail.com>
2016-01-12 16:01:33 +0800

    functioning

---
+++ app/auth-code.js
@@ -0,0 +1,8 @@
+curl --header "Authorization: key=AIzaSyCmX8OsnHG8uXJyhoNaI9rtLkf4myZsHRY" \
+       --header Content-Type:"application/json" \
+       https://gcm-http.googleapis.com/gcm/send \
+       -d "{\"registration_ids\":[\"eTLgxsUF1Wk:APA91bG0PPERBubAcTGe424Fu_FIEwvNTVfx0AqVKZ5z0bm3BbTj7gwZrQl2oYr_mTIXUMrLmlBf4HgCnBkRMlG3zLMVw3sQ1KJLfJM0lvpUX5-9CowZSlV_qeBH5atagG0lOBhcrhaR\"]}"
+
+https://android.googleapis.com/gcm/send/cQ-J_xb4Jzk:APA91bFZRx4qTVR5T6aq-tf12cZ8wBCBHfIEpyDuLhRWhSggv71XM878tFVcG5JHwgdEOLCRYyTrjvtiOoBuew6B3W3zLa2sBCLLWfQNaNKsu6gHb2awlkQYW48ExDEhGLjAbaRffd2V
+
+https://android.googleapis.com/gcm/send/eTLgxsUF1Wk:APA91bG0PPERBubAcTGe424Fu_FIEwvNTVfx0AqVKZ5z0bm3BbTj7gwZrQl2oYr_mTIXUMrLmlBf4HgCnBkRMlG3zLMVw3sQ1KJLfJM0lvpUX5-9CowZSlV_qeBH5atagG0lOBhcrhaR
--- auth-code.js
+++ auth-code.js
@@ -1,7 +1,6 @@
 curl --header "Authorization: key=AIzaSyCmX8OsnHG8uXJyhoNaI9rtLkf4myZsHRY" \
        --header Content-Type:"application/json" \
        https://gcm-http.googleapis.com/gcm/send \
-       -d "{\"registration_ids\":[\"fQ5o5vH6xGY:APA91bE80VHyZSZzODnhlzd780AT8fK-ybImp5mBsJZEUbYYxIZfbh7crKq5oo4jV3E6reVS0w4Db7t--DWQe6zSTp31fToUTaSz5p3JCiLeIZ8mTsahQ_XevwTNsGh4V07dtwG4h-5I\"]}"
+       -d "{\"registration_ids\":[\"eU67liuxsRc:APA91bGfmNH-V7HLZsQ6zX7OBYBb84eB0fzulxUmSuRcGRvvGUOzTHRgOgzYz88a_M2AmhKQtT7JX-h4krxJkdl155LOnV05U3A7s_tBPYMhamzgxi88cDvuaUFLj8mZMIbSrKZiwAQ4\"]}"
 
-
-https://android.googleapis.com/gcm/send/fQ5o5vH6xGY:APA91bE80VHyZSZzODnhlzd780AT8fK-ybImp5mBsJZEUbYYxIZfbh7crKq5oo4jV3E6reVS0w4Db7t--DWQe6zSTp31fToUTaSz5p3JCiLeIZ8mTsahQ_XevwTNsGh4V07dtwG4h-5I
+https://android.googleapis.com/gcm/send/eU67liuxsRc:APA91bGfmNH-V7HLZsQ6zX7OBYBb84eB0fzulxUmSuRcGRvvGUOzTHRgOgzYz88a_M2AmhKQtT7JX-h4krxJkdl155LOnV05U3A7s_tBPYMhamzgxi88cDvuaUFLj8mZMIbSrKZiwAQ4
